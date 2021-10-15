ac7103d6303c60ded00f9ef954fe44757c40e5fd
Mario Miranda <mario.luiz.miranda@gmail.com>
2021-10-15 11:48:19 -0400

    Exceptions, Validations and Security infra

--- backend/src/main/java/com/devsuperior/dslearnbds/repositories/UserRepository.java
+++ backend/src/main/java/com/devsuperior/dslearnbds/repositories/UserRepository.java
@@ -3,9 +3,10 @@ package com.devsuperior.dslearnbds.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.devsuperior.dslearnbds.entities.Topic;
+import com.devsuperior.dslearnbds.entities.User;
 
 @Repository
-public interface UserRepository extends JpaRepository<Topic, Long>{
-
+public interface UserRepository extends JpaRepository<User, Long>{
+	
+	User findByEmail(String email);
 }
